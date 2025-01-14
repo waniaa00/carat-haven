ad40400e698a88599128fedd6757144cb78a74ba
wania akram <160944972+waniaa00@users.noreply.github.com>
2025-01-14 17:27:27 +0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -87,18 +87,7 @@ const Footer = () => {
         <div className="bg-black bg-opacity-75">
           <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
             <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-              {/* <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-              </svg> */}<Image
+             <Image
                             src="/images/logo.jpg"
                             alt="logo"
                             width={50}
